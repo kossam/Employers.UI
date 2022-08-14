716780c688a6d479b818406143eb88093c74d9d8
kossam <kossam@bantusoft.co.za>
2022-08-14 09:29:18 +0200

    employees.angular

---
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
+
+@Component({
+  selector: 'app-employees-list',
+  templateUrl: './employees-list.component.html',
+  styleUrls: ['./employees-list.component.css']
+})
+export class EmployeesListComponent implements OnInit {
+
+  employees: Employee[] = []
+  constructor(private employeeService: EmployeesService) { }
+
+  ngOnInit(): void {
+    this.employeeService.getAllEmployees().subscribe((response)=>{
+      console.log(response);
+      this.employees = response;
+    })
+  }
+
+}
